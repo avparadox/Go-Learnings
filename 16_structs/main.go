94cb72c823d5519d6fa0e096c9b36bf899614a78
avparadox <avyas8927@gmail.com>
2025-01-07 17:46:01 +0530

    Nil or Zero Value Docs for golang

--- 16_structs/main.go
+++ 16_structs/main.go
@@ -22,27 +22,48 @@ func (a order) getAmount () float32 {
 
 
 func main (){
+
+	// If you don't set any of the fields, then default values are enforced on the print statement for the specific variable/object.
+
+	// Int -> 0
+	// String -> Empty space " "
+	
+	// 	In Go, nil represents the zero value for several types, including:
+	// Pointers: A pointer that doesn't point to anything is nil.
+	// Slices: An empty slice is represented by nil.
+	// Maps: An empty map is nil.
+	// Channels: A channel that hasn't been initialized is nil.
+	// Functions: A function that hasn't been assigned a value is nil.
+	// Interfaces: An interface variable that doesn't hold any value is nil.
+
+// 	Not all zero values are nil:
+	// For example, the zero value of an integer is 0, not nil. Similarly, the zero value of a string is "" (an empty string), not nil.
+// Comparing with nil:
+	// You can use the == operator to check if a variable of the above types is nil
+
+
 	myOrder := order{
 		id : "1",
-		amount: 100.00,
+		// amount: 100.00,
 		status: "Delivered",		
 	}
 	
 	// Phase 1 Started
 	// Created sepeartely
-	myOrder.createdAt = time.Now()
-	println("")
-	fmt.Println("Total Order Details: ", myOrder)
-	println("")
-	println("Specific Details: ")
-	println("")
-	fmt.Println("Order id: ", myOrder.id)
-	fmt.Println("Order amount: ", myOrder.amount)
-	fmt.Println("Order status: ", myOrder.status)
-	fmt.Println("Order createdAt: ", myOrder.createdAt)
-	println("")
-
+	// myOrder.createdAt = time.Now()
+	// println("")
+	// fmt.Println("Total Order Details: ", myOrder)
+	// println("")
+	// println("Specific Details: ")
+	// println("")
+	// fmt.Println("Order id: ", myOrder.id)
+	// fmt.Println("Order amount: ", myOrder.amount)
+	// fmt.Println("Order status: ", myOrder.status)
+	// fmt.Println("Order createdAt: ", myOrder.createdAt)
+	// println("")
 	// Phase 1 Ended
+
+
 	// Phase 2 Started
 
 	myOrder2 := order{
@@ -52,18 +73,20 @@ func main (){
 		createdAt: time.Now(),
 	}
 
-	myOrder.amount = 266;
+	// myOrder.amount = 266;
 	fmt.Println(myOrder)
 	fmt.Println(myOrder2)
 
 	// Phase 2 Ended
 
 	// Phase 3
-
-
 	myOrder.changeStatus("Confirmed")
 	fmt.Println(myOrder)
 	fmt.Println(myOrder.status)
 	fmt.Println(myOrder.getAmount())
+	// Phase 3 Ended
+
+
+
 
 }
