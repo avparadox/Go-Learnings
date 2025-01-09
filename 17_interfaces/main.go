694ae263d52273b1a437f13ff7676d18d8e2a623
avparadox <avyas8927@gmail.com>
2025-01-09 18:35:59 +0530

    struct & funcs before interface

--- 17_interfaces/main.go
+++ 17_interfaces/main.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
+type payment struct{}
+
+func (p payment) makePayment (amount float32){
+    // razorPaymentGw := razorPay{}
+    // razorPaymentGw.pay(amount)
+    razorPaymentGw := stripe{}
+    razorPaymentGw.pay(amount)
+}
+
+type razorPay struct{}
+
+func (r razorPay) pay (amount float32){
+    // Logic to make payment
+    fmt.Println("Making payment using RazorPay", amount)
+}
+
+type stripe struct{}
+
+func (s stripe) pay  (amount float32){
+    // Logic to make payment
+    fmt.Println("Making payment using Stripe", amount)
+}
+
 func main()  {
-	
+    newPayment := payment{}
+    newPayment.makePayment(100)
 }
