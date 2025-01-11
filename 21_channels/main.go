d046747dce4c9f5545894b2ffc00a7bcaa00ff12
avparadox <avyas8927@gmail.com>
2025-01-11 13:41:44 +0530

    Sending & Receing messages via channels

---
+++ 21_channels/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+//sending
+// func processNum (numChan chan int){
+// 	for num := range numChan{
+// 		fmt.Println("Processing", num)
+// 		time.Sleep(time.Second * 1)
+// 	}
+// }
+
+//receive
+func sum (result chan int, num1 int, num2 int){
+	finalResult := num1 + num2
+	result <- finalResult
+}
+
+
+func main(){
+
+	result := make(chan int)
+	go sum(result,4,5)
+
+	res := <- result // blocking
+
+	fmt.Println(res)
+
+	// numChan := make(chan int)
+	// go processNum(numChan)
+
+	// for{
+	// 	numChan <- rand.Intn(100)
+	// }
+
+	// messageChan := make(chan string)
+	// messageChan <- "ping & pong" // Blocking Operation
+	// msg := <-messageChan
+	// fmt.Println(msg)
+}
