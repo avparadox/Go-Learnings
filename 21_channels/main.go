7f5f16ede9f93c2a8f0983f0248755f89ae896f1
avparadox <avyas8927@gmail.com>
2025-01-11 14:27:26 +0530

    Email Channel

--- 21_channels/main.go
+++ 21_channels/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 //sending
 // func processNum (numChan chan int){
@@ -17,19 +20,51 @@ import "fmt"
 // }
 
 // goroutine synchronizer
-func task (done chan bool){
-	defer func ()  {
-		done <- true	
-	}()
-	fmt.Println("Processing...")
-}
+// func task (done chan bool){
+// 	defer func ()  {
+// 		done <- true
+// 	}()
+// 	fmt.Println("Processing...")
+// }
+
+
+//Email Channel
+// func emailSender(emailChan chan string, done chan bool){
+
+// 	defer func ()  {
+// 		done <- true	
+// 	}()
+
+// 	for email := range emailChan{
+// 		fmt.Println("Sending emails to", email)
+// 		time.Sleep(time.Second)
+// 	}
+// }
 
 func main(){
 
-	done := make(chan bool)
-	go task(done)
+	// Email Channel
+	// emailChan := make(chan string, 100)
 
-	<- done // blocking
+	// done := make(chan bool)
+
+	// go emailSender(emailChan, done)
+
+	// for i := 0; i < 6 ; i ++{
+	// 	emailChan <- fmt.Sprintf("%d@gmail.com", i)
+	// }
+
+	// fmt.Println("Done Processing")
+	// close(emailChan)
+	// <- done
+	// emailChan <- "1@demo.com"
+	// emailChan <- "2@demo.com"
+
+
+	// done := make(chan bool)
+	// go task(done)
+
+	// <- done // blocking
 
 
 	// result := make(chan int)
