06ec0d9fb9d3c768bf9383111130adae30ae33e5
avparadox <avyas8927@gmail.com>
2025-01-14 18:58:49 +0530

    reading file data using buffer and for loop

--- 23_files/main.go
+++ 23_files/main.go
@@ -1,28 +1,61 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 func main(){
+	// f, err := os.Open("example.txt")
+	// if err != nil {
+	// 	// error handling / logging the error
+	// 	panic(err)
+	// }
+
+	// fileInfo, err := f.Stat()
+	// if err != nil {
+	// 	// error handling / logging the error
+	// 	panic(err)
+	// }
+
+	// fmt.Println("file name: ", fileInfo.Name())
+	// fmt.Println("file true or dir false: ",fileInfo.IsDir())
+	// fmt.Println("file size: ",fileInfo.Size(), " bytes")
+	// fmt.Println("file permissions: ",fileInfo.Mode())
+	// fmt.Println("file modified: ",fileInfo.ModTime())
+	// f, err := os.Open("example.txt")
+	// if err != nil {
+	// 	// error handling / logging the error
+	// 	panic(err)
+	// }
+
+	// fileInfo, err := f.Stat()
+	// if err != nil {
+	// 	// error handling / logging the error
+	// 	panic(err)
+	// }
+
+	// fmt.Println("file name: ", fileInfo.Name())
+	// fmt.Println("file true or dir false: ",fileInfo.IsDir())
+	// fmt.Println("file size: ",fileInfo.Size(), " bytes")
+	// fmt.Println("file permissions: ",fileInfo.Mode())
+	// fmt.Println("file modified: ",fileInfo.ModTime())
+
+	// read file
 	f, err := os.Open("example.txt")
-	if err != nil {
-		// error handling / logging the error
+	if err != nil{
 		panic(err)
 	}
 
-	fileInfo, err := f.Stat()
-	if err != nil {
-		// error handling / logging the error
+	defer f.Close()
+
+	buff := make([]byte, 110)
+	d, err := f.Read(buff)
+	if err != nil{
 		panic(err)
 	}
 
-	fmt.Println("file name: ", fileInfo.Name())
-	fmt.Println("file true or dir false: ",fileInfo.IsDir())
-	fmt.Println("file size: ",fileInfo.Size(), " bytes")
-	fmt.Println("file permissions: ",fileInfo.Mode())
-	fmt.Println("file modified: ",fileInfo.ModTime())
-
+	for i := 0 ; i < len(buff) ; i++{
+		println("data", d,string(buff[i]))
+	}
 
 }
