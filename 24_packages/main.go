a99c3976e40bc7bb3487fd3449e9b7ffe55ffeab
avparadox <avyas8927@gmail.com>
2025-01-15 22:58:01 +0530

    Private/Pubic Packages

--- 24_packages/main.go
+++ 24_packages/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/avparadox/Go-Learnings/auth"
+import (
+	"fmt"
+
+	"github.com/avparadox/Go-Learnings/auth"
+	"github.com/avparadox/Go-Learnings/user"
+)
 
 func main(){
 	auth.LoginWithCredentials("Aditya", "secret")
+	session := auth.GetSession()
+	fmt.Println(session)
+
+	user := user.User{
+		Email: "user@email.com",
+		Name: "John Doe",
+	}
+
+	fmt.Println(user.Email)
+	fmt.Println(user.Name)
+
 }
