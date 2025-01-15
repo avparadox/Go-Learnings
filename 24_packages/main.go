6afb336c2bc8968e392c6beaefa2fb6d0a2519fe
avparadox <avyas8927@gmail.com>
2025-01-15 23:19:17 +0530

    External Packages Installation and more

--- 24_packages/main.go
+++ 24_packages/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/avparadox/Go-Learnings/auth"
 	"github.com/avparadox/Go-Learnings/user"
+	"github.com/fatih/color"
 )
 
 func main(){
@@ -17,7 +18,10 @@ func main(){
 		Name: "John Doe",
 	}
 
-	fmt.Println(user.Email)
-	fmt.Println(user.Name)
+	// fmt.Println(user.Email)
+	// fmt.Println(user.Name)
+	color.Red(user.Email)
+
+// go mod tidy - command to make mod file proper and install the remaining external packages 	
 
 }
